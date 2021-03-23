670a44484cbe4c3e1c5c82480de78326f88ce4f5
Navindra Nulchand <navindra.nulchand@lc.cuny.edu>
2021-03-23 17:41:57 -0400

    modified App.js to say Navindra Nulchand

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          My name is Navindra Nulchand and I will master React!
         </p>
         <a
           className="App-link"
