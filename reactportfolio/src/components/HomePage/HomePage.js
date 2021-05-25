c640e18b89f8bf3efa2dbb66e9709ad88fb38907
Navindra Nulchand <navindra.nulchand@lc.cuny.edu>
2021-05-25 00:40:31 -0400

    Added HomeContent

---
+++ reactportfolio/src/components/HomePage/HomePage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Home = () => { 
+    return (
+        <div className = 'Home'>
+            <div className = 'HomeContent'>
+                <h1>Hello,</h1>
+                <h1>My name is Navindra Nulchand</h1>
+                <h1>Welcome to my Portfolio</h1>
+            </div>
+        </div>
+    )
+}
